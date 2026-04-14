889d9914439f91050eb982a5de472cd57b9a5464
ruval195 <diego.ruvalcaba11@gmail.com>
2026-04-13 19:15:07 -0600

    Remove attachment

--- main.tf
+++ main.tf
@@ -79,12 +79,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
@@ -99,7 +93,7 @@ module "blog_autoscaling" {
   launch_template_name = "blog"
   security_groups = [moudle.blog_sg.security_group_id]
   instance_type = var.instance_type
-  ami           = data.aws_ami.app_ami.id
+  image_id      = data.aws_ami.app_ami.id
 
   traffic_source_attachments = {
     blog_alb = {
